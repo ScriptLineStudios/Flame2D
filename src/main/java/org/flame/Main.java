03222c58432e5837d7a5eb585931e3ad6500f447
butterYeeter <fajandarraaid@gmail.com>
2023-01-20 15:32:19 +0200

    Shaders part 2

--- src/main/java/org/flame/Main.java
+++ src/main/java/org/flame/Main.java
@@ -9,11 +9,13 @@ public class Main {
 	public static void main(String[] args)
 	{
 		Engine e = new Engine("test", 303, 300);
+		Texture t = new Texture("");
 
 		e.clear(1.0f, 1.0f, 0.0f, 1.0f);
 
 		while(!glfwWindowShouldClose(e.window))
 		{
+			t.render();
 			e.update();
 		}
 	}
