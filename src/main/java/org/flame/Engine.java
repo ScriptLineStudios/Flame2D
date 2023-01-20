d01bd22d440f5d126d4379c6811d0f2eaea7b17c
butterYeeter <fajandarraaid@gmail.com>
2023-01-20 15:32:03 +0200

    Shaders part 2

--- src/main/java/org/flame/Engine.java
+++ src/main/java/org/flame/Engine.java
@@ -35,7 +35,6 @@ public class Engine
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 4);
         glfwSwapBuffers(this.window);
         glfwPollEvents();
     }
--- src/main/java/org/flame/Texture.java
+++ src/main/java/org/flame/Texture.java
@@ -22,38 +22,38 @@ public class Texture
             -0.5f, 0.5f, 0.0f
         };
 
-    int shadProg;
+        int shadProg;
 
-    int vertShad = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertShad, "src/main/resources/vertex.glsl");
-    glCompileShader(vertShad);
+        int vertShad = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertShad, "src/main/resources/vertex.glsl");
+        glCompileShader(vertShad);
 
-    int fragShad = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragShad, "src/main/resources/fragment.glsl");
-    glCompileShader(fragShad);
+        int fragShad = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragShad, "src/main/resources/fragment.glsl");
+        glCompileShader(fragShad);
 
-    shadProg = glCreateProgram();
-    glAttachShader(shadProg, vertShad);
-    glAttachShader(shadProg, fragShad);
-    glLinkProgram(shadProg);
+        shadProg = glCreateProgram();
+        glAttachShader(shadProg, vertShad);
+        glAttachShader(shadProg, fragShad);
+        glLinkProgram(shadProg);
 
-    int vao, vbo;
+        int vao, vbo;
 
-    vao = glGenVertexArrays();
-    glBindVertexArray(vao);
+        vao = glGenVertexArrays();
+        glBindVertexArray(vao);
 
-    FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertices.length);
-    vertexBuffer.put(vertices).flip();
+        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertices.length);
+        vertexBuffer.put(vertices).flip();
 
-    vbo = glGenBuffers();
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER,vertexBuffer, GL_STATIC_DRAW);
+        vbo = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER,vertexBuffer, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, false, 3, 3 * Float.BYTES);
-    glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 3, 3 * Float.BYTES);
+        glEnableVertexAttribArray(0);
 
 
-    glUseProgram(shadProg);
+        glUseProgram(shadProg);
 
 
     }
@@ -62,5 +62,11 @@ public class Texture
     public static String loadAsString(String filePath) throws IOException
     {
         return Files.readString(Path.of(filePath));
+    }
+
+
+    public static void render()
+    {
+        glDrawArrays(GL_TRIANGLES, 0, 4);
     }
 }
