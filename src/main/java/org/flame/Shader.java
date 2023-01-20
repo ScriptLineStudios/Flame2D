2dfd97e252124c2120d063c543f351ee99d1a746
butterYeeter <fajandarraaid@gmail.com>
2023-01-20 20:28:21 +0200

    added shader class

---
+++ src/main/java/org/flame/Shader.java
@@ -0,0 +1,33 @@
+package org.flame;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.lwjgl.opengl.GL20.*;
+
+public class Shader
+{
+    int shaderProgram;
+
+    Shader(String vPath, String fPath) throws IOException
+    {
+        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, loadAsString(vPath));
+        glCompileShader(vertexShader);
+
+        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, loadAsString(fPath));
+        glCompileShader(fragmentShader);
+
+        this.shaderProgram = glCreateProgram();
+        glAttachShader(this.shaderProgram, vertexShader);
+        glAttachShader(this.shaderProgram, fragmentShader);
+        glLinkProgram(this.shaderProgram);
+    }
+
+    public static String loadAsString(String filePath) throws IOException
+    {
+        return Files.readString(Path.of(filePath));
+    }
+}
--- src/main/java/org/flame/Texture.java
+++ src/main/java/org/flame/Texture.java
@@ -12,30 +12,19 @@ import static org.lwjgl.opengl.GL30.*;
 
 public class Texture
 {
-    int vao, vbo, shaderProgram;
+    int vao, vbo;
+    Shader s;
+
     Texture(String path) throws IOException
     {
+        s = new Shader("src/main/resources/vertex.glsl", "src/main/resources/fragment.glsl");
         this.vao = glGenVertexArrays();
         this.vbo = glGenBuffers();
 
-        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertexShader, loadAsString("src/main/resources/vertex.glsl"));
-        glCompileShader(vertexShader);
 
-        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragmentShader, loadAsString("src/main/resources/fragment.glsl"));
-        glCompileShader(fragmentShader);
-
-        this.shaderProgram = glCreateProgram();
-        glAttachShader(this.shaderProgram, vertexShader);
-        glAttachShader(this.shaderProgram, fragmentShader);
-        glLinkProgram(this.shaderProgram);
     }
     
-    public static String loadAsString(String filePath) throws IOException
-    {
-        return Files.readString(Path.of(filePath));
-    }
+
 
     public void render() throws IOException
     {
@@ -58,7 +47,7 @@ public class Texture
         glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * Float.BYTES, 0);
         glEnableVertexAttribArray(0);
 
-        glUseProgram(this.shaderProgram);
+        glUseProgram(s.shaderProgram);
         glBindVertexArray(this.vao);
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
