4d4c0d1f63f6c2411cadfdcd7f9f8629793641b5
ScriptLineStudios <scriptlinestudios@protonmail.com>
2023-01-21 14:13:47 +0200

    added joml

--- bin/main/fragment.glsl
+++ bin/main/fragment.glsl
@@ -2,9 +2,10 @@
 uniform sampler2D img;
 
 in vec2 outCoord;
-uniform sampler2D img;
+//uniform sampler2D img;
 
 void main()
 {
     gl_FragColor = texture(img, outCoord);
+//    gl_FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);
 }
